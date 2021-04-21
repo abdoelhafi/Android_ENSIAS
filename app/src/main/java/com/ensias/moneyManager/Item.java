4fb3979e6bf6bc78c168abb060bf8fa92afb33c4
AyoubHmadouch <ayoub_hmadouch@um5.ac.ma>
2021-04-21 11:55:14 +0000

    update project

---
+++ app/src/main/java/com/ensias/moneyManager/Item.java
@@ -0,0 +1,39 @@
+package com.ensias.moneyManager;
+
+import java.io.Serializable;
+
+
+
+public class Item implements Serializable {
+    public static final String TYPE_EXPENSE = "expense";
+    public static final String TYPE_INCOME = "income";
+    public static final String TYPE_UNKNOWN = "unknown";
+
+    private String mName;
+    private double mPrice;
+    private String mType;
+    private int mId;
+
+    public Item (String name, double price, String type, int id) {
+        mName = name;
+        mPrice = price;
+        mType = type;
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public double getPrice() {
+        return mPrice;
+    }
+
+    public int getId() {
+        return mId;
+    }
+}
